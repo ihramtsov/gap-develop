010e92df598c2b4da3321dba426d5f485343b9d2
Igor <ihramtsov@gmail.com>
2014-04-15 00:02:52 +0600

    перетащил подсветку синтаксиса гапа в отдельный плагин

---
+++ MonoDevelop.GapHighlighting/ConfigureHighlightingCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Mono.TextEditor.Highlighting;
+using MonoDevelop.Components.Commands;
+
+namespace MonoDevelop.GapHighlighting
+{
+    public class ConfigureHighlightingCommandHandler : CommandHandler
+    {
+        protected override void Run()
+        {
+            SyntaxModeService.LoadStylesAndModes (this.GetType ().Assembly);
+        }
+    }
+}
