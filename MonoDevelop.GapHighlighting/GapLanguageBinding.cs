dd45a41a88d39546191ab76308f804b942a334b5
issilin <kirillyakunin7@gmail.com>
2014-06-16 12:58:40 +0500

    подсказка, не знаю зачем это, но Ксюша сказала закоммитить

---
+++ MonoDevelop.GapHighlighting/GapLanguageBinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.Projects;
+using MonoDevelop.Core;
+using System.IO;
+
+namespace MonoDevelop.GapHighlighting
+{
+    class GapLanguageBinding : ILanguageBinding
+    {
+        public string Language
+        {
+            get { return "gap"; }
+        }
+
+        public string SingleLineCommentTag
+        {
+            get { return "#"; }
+        }
+
+        public string BlockCommentStartTag
+        {
+            get { return "#"; }
+        }
+
+        public string BlockCommentEndTag
+        {
+            get { return "#"; }
+        }
+
+        public bool IsSourceCodeFile(FilePath fileName){return StringComparer.OrdinalIgnoreCase.Equals (Path.GetExtension (fileName), ".g");}
+        public FilePath GetFileName(FilePath fileNameWithoutExtension) {return fileNameWithoutExtension + ".g";}
+    }
+}
---
+++ MonoDevelop.GapHighlighting/Help.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.FindInFiles;
+using System.Threading;
+using MonoDevelop.SourceEditor;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui.Content;
+
+using ICSharpCode.NRefactory.CSharp.Resolver;
+
+using ICSharpCode.NRefactory.Semantics;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.Completion;
+using MonoDevelop.Ide.CodeCompletion;
+
+namespace MonoDevelop.GapHighlighting
+{
+    class Help : CompletionTextEditorExtension 
+    {
+        ICompletionDataList InternalHandleCodeCompletion(CodeCompletionContext completionContext, bool ctrlSpace) {
+            CompletionDataList list = new CompletionDataList();
+            list.AutoCompleteEmptyMatchOnCurlyBrace = true;
+            list.Add("Подсказка =)");
+            return list;
+        }
+        public override string CompletionLanguage
+        {
+            get
+            {
+                return "C#";
+            }
+        }
+
+        public override ICompletionDataList HandleCodeCompletion(CodeCompletionContext completionContext, char completionChar, ref int triggerWordLength)
+        {
+                return InternalHandleCodeCompletion(completionContext, true);
+        }
+
+        public override ICompletionDataList CodeCompletionCommand(CodeCompletionContext completionContext)
+        {
+            
+            return InternalHandleCodeCompletion(completionContext,true);
+        }
+
+        internal Mono.TextEditor.TextEditorData TextEditorData
+        {
+            get
+            {
+                var doc = Document;
+                if (doc == null)
+                    return null;
+                return doc.Editor;
+            }
+        }
+        public new MonoDevelop.Ide.Gui.Document Document
+        {
+            get
+            {
+                return base.document;
+            }
+        }
+	}
+}
