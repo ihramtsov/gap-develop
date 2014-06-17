5a680b54f41f7bc519ccd187e3a0e8da4bb78071
issilin <kirillyakunin7@gmail.com>
2014-06-17 23:15:52 +0500

    подскзка

--- MonoDevelop.GapHighlighting/Help.cs
+++ MonoDevelop.GapHighlighting/Help.cs
@@ -20,30 +20,69 @@ namespace MonoDevelop.GapHighlighting
 {
     class Help : CompletionTextEditorExtension 
     {
-        ICompletionDataList InternalHandleCodeCompletion(CodeCompletionContext completionContext, bool ctrlSpace) {
+
+        ICompletionDataList InternalHandleCodeCompletion(CodeCompletionContext completionContext, char completionChar, bool ctrlSpace, ref int triggerWordLength)
+        {
             CompletionDataList list = new CompletionDataList();
-            list.AutoCompleteEmptyMatchOnCurlyBrace = true;
-            list.Add("Подсказка =)");
-            return list;
+            list.Sort();
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"..\resources\function.txt");
+                foreach (string x in lines)
+                {
+                    list.Add(x);
+                }
+                return list.Count > 0 ? list : null;
+            }
+            catch (Exception e)
+            {
+                LoggingService.LogError("Unexpected code completion exception." + Environment.NewLine +
+                    "FileName: " + Document.FileName + Environment.NewLine +
+                    "Position: line=" + completionContext.TriggerLine + " col=" + completionContext.TriggerLineOffset + Environment.NewLine +
+                    "Line text: " + Document.Editor.GetLineText(completionContext.TriggerLine),
+                    e);
+                return null;
+            }
         }
+
         public override string CompletionLanguage
         {
             get
             {
-                return "C#";
+                return "Gap";
             }
         }
 
         public override ICompletionDataList HandleCodeCompletion(CodeCompletionContext completionContext, char completionChar, ref int triggerWordLength)
         {
-                return InternalHandleCodeCompletion(completionContext, true);
+            //	var timer = Counters.ResolveTime.BeginTiming ();
+            try
+            {
+                if (char.IsLetterOrDigit(completionChar) || completionChar == '_')
+                {
+                    if (completionContext.TriggerOffset > 1 && char.IsLetterOrDigit(document.Editor.GetCharAt(completionContext.TriggerOffset - 2)))
+                        return null;
+                    triggerWordLength = 1;
+                }
+                return InternalHandleCodeCompletion(completionContext, completionChar, false, ref triggerWordLength);
+            }
+            catch (Exception e)
+            {
+                LoggingService.LogError("Unexpected code completion exception." + Environment.NewLine +
+                    "FileName: " + Document.FileName + Environment.NewLine +
+                    "Position: line=" + completionContext.TriggerLine + " col=" + completionContext.TriggerLineOffset + Environment.NewLine +
+                    "Line text: " + Document.Editor.GetLineText(completionContext.TriggerLine),
+                    e);
+                return null;
+            }
+            
         }
 
-        public override ICompletionDataList CodeCompletionCommand(CodeCompletionContext completionContext)
-        {
-            
-            return InternalHandleCodeCompletion(completionContext,true);
-        }
+        //public override ICompletionDataList CodeCompletionCommand(CodeCompletionContext completionContext)
+        //{
+
+        //    return InternalHandleCodeCompletion(completionContext, completionChar, false, ref triggerWordLength);
+        //}
 
         internal Mono.TextEditor.TextEditorData TextEditorData
         {
